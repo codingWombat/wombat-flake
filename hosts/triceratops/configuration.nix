090c7efb61eb1c9e4f2d04e5aa299ad9154f8c4b
Thomas Budde <mail@codingwombat.dev>
2025-01-05 20:23:16 +0100

    intel fixes

--- hosts/triceratops/configuration.nix
+++ hosts/triceratops/configuration.nix
@@ -18,6 +18,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  hardware.enableRedistributableFirmware = true;
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [ intel-media-driver
