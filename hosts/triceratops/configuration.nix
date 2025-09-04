d89ee7c46c0e645af14e54fb955f29670062b5ae
Thomas Budde <mail@codingwombat.dev>
2025-09-04 15:43:02 +0200

    removed unneeded inputs

--- hosts/triceratops/configuration.nix
+++ hosts/triceratops/configuration.nix
@@ -1,14 +1,13 @@
 {
-  lib,
   pkgs,
   inputs,
   ...
 }:
 {
-  boot.initrd.kernelModules = ["i915"];
+  boot.initrd.kernelModules = [ "i915" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.loader = {    
+  boot.loader = {
     systemd-boot = {
       enable = true;
       memtest86.enable = true;
@@ -21,7 +20,8 @@
   hardware.enableRedistributableFirmware = true;
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [ intel-media-driver
+    extraPackages = with pkgs; [
+      intel-media-driver
       intel-ocl
       intel-vaapi-driver
       vpl-gpu-rt
@@ -29,8 +29,12 @@
     ];
   };
 
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "i965"; };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "i965";
+  };
 
+  # install haos as vm
+  # sudo virt-install --connect qemu:///system --name haos --description "Home Assistant OS" --os-variant=generic --ram=4096 --vcpus=2 --disk /opt/data/vms/haos_ova-14.2.qcow2,bus=scsi --controller type=scsi,model=virtio-scsi --import --graphics "spice,listen=0.0.0.0" --network network=bridged-network --boot uefi
   virtualisation = {
     libvirtd = {
       enable = true;
@@ -64,10 +68,17 @@
   networking = {
     hostName = "triceratops";
     firewall = {
-        allowedTCPPorts = [ 5900 8123];
-        checkReversePath = "loose";
+      allowedTCPPorts = [
+        5900
+        8123
+      ];
+      checkReversePath = "loose";
     };
-    nameservers = ["192.168.111.1" "1.1.1.1" "9.9.9.9"];
+    nameservers = [
+      "192.168.111.1"
+      "1.1.1.1"
+      "9.9.9.9"
+    ];
     defaultGateway = {
       address = "192.168.111.1";
       interface = "enp12s0";
@@ -77,10 +88,12 @@
     ### main
     interfaces.enp12s0 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.11";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.11";
+          prefixLength = 24;
+        }
+      ];
     };
 
     bridges.br0.interfaces = [ "enp13s0" ];
@@ -88,45 +101,61 @@
     ### hass os vm
     interfaces.br0 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.12";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.12";
+          prefixLength = 24;
+        }
+      ];
     };
 
     ## 2.5 gig nics
     ### kube metallb
     interfaces.ens1 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.18";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.18";
+          prefixLength = 24;
+        }
+      ];
     };
 
     interfaces.ens9 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.25";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.25";
+          prefixLength = 24;
+        }
+      ];
     };
 
     interfaces.enp5s0 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.26";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.26";
+          prefixLength = 24;
+        }
+      ];
     };
 
     interfaces.enp6s0 = {
       useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.111.27";
-        prefixLength = 24;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.168.111.27";
+          prefixLength = 24;
+        }
+      ];
     };
+  };
+
+  nix.gc = {
+    automatic = true;
+    randomizedDelaySec = "14m";
+    options = "--delete-older-than 10d";
   };
 
   # Necessary for using flakes on this system.
