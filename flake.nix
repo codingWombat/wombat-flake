2e30f0e6818b688e9cc6a497b144526bd6ef5a3b
Thomas Budde <mail@codingwombat.dev>
2025-12-02 18:26:42 +0100

    added new host

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -10,41 +12,81 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nix-darwin, nixpkgs, disko }
-  @inputs:
-  let
-    modules = [(import ./modules/default.nix)];
-    linuxModules = [(import ./modules/linux.nix)];
-    publicKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK5hep9vnJDpydFRLwJhBkwEFSWeA7jLrHAS+liNcasc codingwombat@MacBook-Pro.localdomain"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+wFxNm9hmOYgNkBNH0MU0WBjvY9B1Eff0aYUN4P/t8 workwombat"
-    ];
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#codingwombat
-    darwinConfigurations."codingwombat" = nix-darwin.lib.darwinSystem {
-      inherit inputs;
-      system = "aarch64-darwin";
-      modules = [ ./hosts/codingwombat/configuration.nix ] ++ modules;
-    };
-
-    nixosConfigurations."velociraptor" = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [ ./hosts/velociraptor/configuration.nix
-        ./hosts/velociraptor/disk-config.nix
-        disko.nixosModules.disko
-      ] ++ modules ++ linuxModules;
-      specialArgs = { inherit inputs; inherit publicKeys; };
-    };
-
-    nixosConfigurations."triceratops" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./hosts/triceratops/configuration.nix
-        ./hosts/triceratops/disk-config.nix
-        disko.nixosModules.disko
-      ] ++ modules ++ linuxModules;
-      specialArgs = { inherit inputs; inherit publicKeys; };
-    };
+  nixConfig = {
+    extra-trusted-substituters = [ "https://cache.soopy.moe" ];
+    extra-substituters = [ "https://cache.soopy.moe" ];
+    extra-trusted-public-keys = [ "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=" ];
   };
+
+  outputs =
+    {
+      self,
+      nix-darwin,
+      nixpkgs,
+      nixos-hardware,
+      disko,
+    }@inputs:
+    let
+      modules = [ (import ./modules/default.nix) ];
+      linuxModules = [ (import ./modules/linux.nix) ];
+      t2Modules = [ (import ./mmodules/t2.nix) ];
+      publicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK5hep9vnJDpydFRLwJhBkwEFSWeA7jLrHAS+liNcasc codingwombat@MacBook-Pro.localdomain"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+wFxNm9hmOYgNkBNH0MU0WBjvY9B1Eff0aYUN4P/t8 workwombat"
+      ];
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#codingwombat
+      darwinConfigurations."codingwombat" = nix-darwin.lib.darwinSystem {
+        inherit inputs;
+        system = "aarch64-darwin";
+        modules = [ ./hosts/codingwombat/configuration.nix ] ++ modules;
+      };
+
+      nixosConfigurations."velociraptor" = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./hosts/velociraptor/configuration.nix
+          ./hosts/velociraptor/disk-config.nix
+          disko.nixosModules.disko
+        ]
+        ++ modules
+        ++ linuxModules;
+        specialArgs = {
+          inherit inputs;
+          inherit publicKeys;
+        };
+      };
+
+      nixosConfigurations."triceratops" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/triceratops/configuration.nix
+          ./hosts/triceratops/disk-config.nix
+          disko.nixosModules.disko
+        ]
+        ++ modules
+        ++ linuxModules;
+        specialArgs = {
+          inherit inputs;
+          inherit publicKeys;
+        };
+      };
+
+      nixosConfigurations."stegosaurus" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/stegosaurus/configuration.nix
+              nixos-hardware.nixosModules.apple-t2
+            ]
+            ++ modules
+            ++ t2Modules;
+            specialArgs = {
+              inherit inputs;
+              inherit publicKeys;
+            };
+          };
+        };
+    };
 }
---
+++ hosts/stegosaurus/configuration.nix
@@ -0,0 +1,78 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    efi.efiSysMountPoint = "/boot"; # make sure to change this to your EFI partition!
+    systemd-boot.enable = true;
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  codingwombat.wombatmin.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # helix
+    # wget
+    git
+    # zed-editor
+    # btop
+    # lsd
+    # wireguard-tools
+    intel-gpu-tools
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
+    };
+    openFirewall = true;
+  };
+
+  nix.settings = {
+    substituters = [ "https://cache.soopy.moe" ];
+    trusted-substituters = [ "https://cache.soopy.moe" ]; # to allow building as a non-trusted user
+    trusted-public-keys = [ "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=" ];
+  };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
