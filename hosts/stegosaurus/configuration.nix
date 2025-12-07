1692753e10ed1b52e04032cdb3676de6080dcb80
Thomas Budde <mail@codingwombat.dev>
2025-12-07 11:05:56 +0100

    removed samba mount

--- hosts/stegosaurus/configuration.nix
+++ hosts/stegosaurus/configuration.nix
@@ -36,7 +36,6 @@
   environment.systemPackages = with pkgs; [
     intel-gpu-tools
     helix
-    cifs-utils
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -66,16 +65,6 @@
       PasswordAuthentication = false; # disable password login
     };
     openFirewall = true;
-  };
-
-  fileSystems."/mnt/media" = {
-    device = "//192.168.10.145/Personal-Drive";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=/etc/nixos/.smbcredentials,uid=1000,gid=100"];
   };
 
   nix.settings = {
