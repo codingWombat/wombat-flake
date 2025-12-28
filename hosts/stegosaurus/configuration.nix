93cff861d339ff17387078a14517c998a3f6d9b8
Thomas Budde <mail@codingwombat.dev>
2025-12-28 14:48:08 +0100

    updated network config

--- hosts/stegosaurus/configuration.nix
+++ hosts/stegosaurus/configuration.nix
@@ -46,26 +46,22 @@
   virtualisation.incus.ui.enable = true;
 
   networking.nftables.enable = true;
-
+  networking.bridges.br0.interfaces = [ "enp4s0f0" ];
   networking.hostName = "stegosaurus";
   
   networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
     53
     67
-    8123
-    5900
   ];
   networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
     53
     67
-    8123
-    5900
   ];
 
-  networking.firewall.trustedInterfaces = [ "incusbr0" ];
-
+  networking.firewall.trustedInterfaces = [ "incusbr0" "br0"];
+  networking.firewall.interfaces."br0".allowedTCPPorts = [ 8123 5900];
   networking.firewall.interfaces."enp4s0f0".allowedTCPPorts = [ 5000 8080 ];
-
+  networking.firewall.checkReversePath = "loose";
   users.users.wombatmin.extraGroups = [ "incus-admin" ];
 
   services.openssh = {
