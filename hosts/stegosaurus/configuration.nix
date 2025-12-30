f4acafc6877a89667c707858992183c44e7027ae
Thomas Budde <mail@codingwombat.dev>
2025-12-30 08:53:36 +0100

    added media mount

--- hosts/stegosaurus/configuration.nix
+++ hosts/stegosaurus/configuration.nix
@@ -113,6 +113,18 @@
       [ "${automount_opts},credentials=/etc/nixos/smbpasswd,uid=1000,gid=100" ];
   };
 
+  fileSystems."/mnt/media" = {
+    device = "//192.168.10.145/mediaVault";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/jellycred,uid=1000,gid=100" ];
+  };
+
   nix.settings = {
     substituters = [ "https://cache.soopy.moe" ];
     trusted-substituters = [ "https://cache.soopy.moe" ]; # to allow building as a non-trusted user
